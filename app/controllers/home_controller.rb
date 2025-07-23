7f9a7b1882d2653b5460c8a18529bd6947005ccd
William Comartin <wcomartin@alterisgroup.com>
2025-07-23 01:20:51 -0400

    Add damage and chaos roll display to home page
    Introduced two new UI elements to display damage and chaos rolls on the home page. These values are generated in the `HomeController` using `SecureRandom`. Updated the layout to include these elements alongside the Next Turn button for better user interaction.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @damage_roll = SecureRandom.random_number(0..9)
+    @chaos_roll = (SecureRandom.random_number(0..19) * 10) + SecureRandom.random_number(0..9)
   end
 end
